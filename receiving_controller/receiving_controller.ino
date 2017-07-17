bf9849521bb78f03f3120ed3769031f78c6dd567
kelawaad <karim.elawaad@hotmail.com>
2017-07-17 02:38:38 +0200

    first commit

---
+++ receiving_controller/receiving_controller.ino
@@ -0,0 +1,86 @@
+#include <IRremote.h>
+#include <IRremoteInt.h>
+#include <U8glib.h>
+#include <stdlib.h>
+
+#define RECV_PIN 2
+#define WHEEL_RADIUS 0.29f
+#define NUMBER_OF_MAGNETS 1
+
+#define DIN_PIN 4
+#define DC_PIN 5
+#define RST_PIN 6
+#define CE_PIN 7
+#define CLK_PIN 8
+#define BACKLIGHT_PIN 11
+
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+U8GLIB_PCD8544 disp(CLK_PIN, DIN_PIN, CE_PIN, DC_PIN, RST_PIN);
+
+uint8_t currentCyclesCount;
+uint32_t cyclesCount;
+float currentSpeed;
+uint64_t lastUpdateTime;
+
+void setup() {
+  // put your setup code here, to run once:
+  currentCyclesCount = 0;
+  cyclesCount = 0;
+  currentSpeed = 0;
+
+  analogWrite(BACKLIGHT_PIN, 255);
+  
+  Serial.begin(9600);
+  Serial.println("Starting..");
+
+  irrecv.enableIRIn();  
+
+  updateDisplay();
+  lastUpdateTime = millis();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(irrecv.decode(&results))
+  {
+    currentCyclesCount = results.value;
+    cyclesCount += currentCyclesCount;
+    currentSpeed = currentCyclesCount * WHEEL_RADIUS * 6.28318530716f * 3.6f / (NUMBER_OF_MAGNETS * 2);
+    disp.firstPage();
+    do {
+    updateDisplay();
+    }while(disp.nextPage());
+    Serial.print("Current Cycles Count: ");
+    Serial.print(currentCyclesCount);
+    Serial.print("\tTotal Cycles Count: ");
+    Serial.println(cyclesCount);
+    Serial.print(currentSpeed);
+    Serial.println(" KM/H");
+
+    lastUpdateTime = millis();
+    irrecv.resume();
+  }
+}
+
+void updateDisplay()
+{
+//  disp.firstPage();
+//  disp.setFont(u8g_font_freedoomr25n);  // select font
+//  disp.setPrintPos(15, 30);  // set position
+//  
+//  //FloatToStringNew(currentSpeed, currentVelocity, 10);
+//  snprintf(currentSpeedStr, 10, "%f", currentSpeed);
+//  disp.drawStr(0, 25, dtostrf(currentSpeed, 5, 2, currentSpeedStr));
+//  disp.drawStr(50, 15, "km/hr ");
+  char currentSpeedStr[10];
+  disp.firstPage();
+  do{
+  disp.setFont(u8g_font_fur25);  // select font
+  snprintf(currentSpeedStr, 10, "%.1f", currentSpeed);
+  disp.drawStr(0, 35, dtostrf(currentSpeed, 5, 2, currentSpeedStr));
+  //disp.setPrintPos(15, 15);  // set position
+  }while(disp.nextPage());
+}
+
---
+++ sending_controller/sending_controller.ino
@@ -0,0 +1,58 @@
+/*
+ * Sends a signal each time the bike completes one cycle using IR communication.
+ * A complete cycle occurs each time the reed switch is activated.
+ * Two possible approaches:
+ *    1- Send a signal each time a cycle completes
+ *    2- Send the total number of cycles each 1 second
+ *    
+ *    Connection:
+ *    2- Reed Switch
+ *    3- IR Led
+ */
+
+#include <IRremote.h>
+#include <IRremoteInt.h>
+
+#define REED_PIN 2
+
+IRsend irsend;
+
+volatile uint8_t cyclesCount;
+uint64_t lastSendTime;
+
+void setup() {
+  // put your setup code here, to run once:
+  cli();
+//  TCCR1A = 0;// set entire TCCR1A register to 0
+//  TCCR1B = 0;// same for TCCR1B
+//  TCNT1  = 0;//initialize counter value to 0
+//  OCR1A = 15624;
+//  TCCR1B |= (1 << WGM12);  // turn on CTC mode
+//  TCCR1B |= (1 << CS12) | (1 << CS10);  // Set prescaler to 1024
+//  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
+  pinMode(REED_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(REED_PIN), incrementCycles, RISING);
+  cyclesCount = 0;
+  lastSendTime = millis();
+  sei();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(millis() - lastSendTime >= 1000)
+  {
+    irsend.sendRC6(cyclesCount, 16);
+    cyclesCount = 0;
+    lastSendTime = millis();
+  }
+}
+
+void incrementCycles(){
+  cyclesCount++;
+}
+
+ISR(TIMER1_COMPA_vect){
+  irsend.sendRC6(cyclesCount, 16);
+  cyclesCount = 0;
+}
+
